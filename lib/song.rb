09c0bca41693dd80c7caf68ad77bc75fe598422c
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2023-02-27 16:58:28 +0300

    fixed solution

--- lib/song.rb
+++ lib/song.rb
@@ -1,165 +1,49 @@
 class Song
-  attr_accessor :name, :artist, :genre
-  @@count = 0
-  @@artists = []
-  @@genres = []
+    attr_accessor :name, :artist , :genre
 
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@count += 1
-    @@artists << artist
-    @@genres << genre
-  end
+    def initialize (name,artist,genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
 
-  def self.count
-    @@count
-  end
-  def self.artists
-    attr_reader :artist
-    @@artists.uniq
-  end
-  def self.genres
-    attr_reader :genre
-    @@genres.uniq
-  end
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def self.count
+        @@count 
     end
-    genre_count
-  end
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
+
+    def self.genres
+        @@genres.uniq
     end
-    artist_count
-  end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+    
 end
 
-# Path: lib/artist.rb
-class Artist
-  attr_accessor :name
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
-  end
-
-  def add_song(song)
-    song.artist = self
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    add_song(song)
-  end
-
-  def self.song_count
-    Song.all.count
-  end
-endclass Song
-  attr_accessor :name, :artist, :genre
-  @@count = 0
-  @@artists = []
-  @@genres = []
-
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@count += 1
-    @@artists << artist
-    @@genres << genre
-  end
-
-  def self.count
-    @@count
-  end
-  def self.artists
-    attr_reader :artist
-    @@artists.uniq
-  end
-  def self.genres
-    attr_reader :genre
-    @@genres.uniq
-  end
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
-end
-
-# Path: lib/artist.rb
-class Artist
-  attr_accessor :name
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
-  end
-
-  def add_song(song)
-    song.artist = self
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    add_song(song)
-  end
-
-  def self.song_count
-    Song.all.count
-  end
-end
+p ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+p song2 = Song.new("Being a woman","carolina jones","country")
+p song2 = Song.new("What a View","carolina jones","country")
+p ninety_nine_problems.artist
+p ninety_nine_problems.genre
+p Song.count
+p Song.artists
+p Song.genres
+p Song.genre_count
+p Song.artist_count
